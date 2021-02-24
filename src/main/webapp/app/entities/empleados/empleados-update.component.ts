2e7e300ed4fb6979d3e09fd79e5778e2717aa4e0
ArielVola <ariel18vola@gmail.com>
2021-02-24 01:08:01 -0300

    LA CONCHA DE LA GORRA

--- src/main/webapp/app/entities/empleados/empleados-update.component.ts
+++ src/main/webapp/app/entities/empleados/empleados-update.component.ts
@@ -6,9 +6,9 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 
-import { IEmpleados, Empleados } from 'app/shared/model/empleados.model';
+import { IEmpleados } from 'app/shared/model/empleados.model';
 import { EmpleadosService } from './empleados.service';
-import { IUsuarios, Usuarios } from 'app/shared/model/usuarios.model';
+import { IUsuarios } from 'app/shared/model/usuarios.model';
 import { UsuariosService } from 'app/entities/usuarios/usuarios.service';
 import { IEstados } from 'app/shared/model/estados.model';
 import { EstadosService } from 'app/entities/estados/estados.service';
@@ -22,15 +22,12 @@ import { FichajesService } from 'app/entities/fichajes/fichajes.service';
 import { IEmpresas } from 'app/shared/model/empresas.model';
 import { EmpresasService } from 'app/entities/empresas/empresas.service';
 import { ITipoDocumentos } from 'app/shared/model/tipo-documentos.model';
-import { IPaises, Paises } from 'app/shared/model/paises.model';
-import { IProvincias, Provincias } from 'app/shared/model/provincias.model';
-import { Ciudades, ICiudades } from 'app/shared/model/ciudades.model';
+import { IPaises } from 'app/shared/model/paises.model';
+import { IProvincias } from 'app/shared/model/provincias.model';
+import { ICiudades } from 'app/shared/model/ciudades.model';
 import { PaisesService } from '../paises/paises.service';
 import { ProvinciasService } from '../provincias/provincias.service';
 import { CiudadesService } from '../ciudades/ciudades.service';
-import { ITipoContactos, TipoContactos } from 'app/shared/model/tipo-contactos.model';
-import { Direcciones, IDirecciones } from 'app/shared/model/direcciones.model';
-import { EmpleadoRegistroDto } from 'app/shared/model/empleado-registro-dto.model';
 
 type SelectableEntity = IEmpleados | IUsuarios | IEstados | IAreas | IPuestos | IFichajes | IEmpresas;
 
@@ -53,61 +50,9 @@ export class EmpleadosUpdateComponent implements OnInit {
   tipodocumentos?: ITipoDocumentos[];
   fechaIngresoDp: any;
 
-  editForm = this.fb.group({
-    id: [],
-    fechaIngreso: [],
-    jefe: [],
-    usuario: [],
-    estado: [],
-    area: [],
-    puesto: [],
-    fichaje: [],
-    empresa: [],
-  });
+  empleadoSeleccionado?: any;
 
-  usuarioForm = this.fb.group({
-    id: [],
-    nombre: [],
-    apellido: [],
-    fechaNac: [],
-    clave: [],
-    usuario: [],
-    estado: [],
-    direccion: [],
-    contacto: [],
-  });
-
-  contactoForm = this.fb.group({
-    id: [],
-    valorDocumento: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]],
-    tipoDocumentos: ['', [Validators.required]],
-  });
-
-  paisForm = this.fb.group({
-    id: [],
-    nombre: ['', [Validators.required]],
-  });
-
-  provinciaForm = this.fb.group({
-    id: [],
-    nombre: ['', [Validators.required]],
-    pais: ['', [Validators.required]],
-  });
-
-  ciudadForm = this.fb.group({
-    id: [],
-    nombre: ['', [Validators.required]],
-    provincia: ['', [Validators.required]],
-  });
-
-  direccionesForm = this.fb.group({
-    id: [],
-    calle: ['', [Validators.required]],
-    altura: ['', [Validators.required]],
-    piso: ['', [Validators.required]],
-    departamento: ['', [Validators.required]],
-    ciudad: ['', [Validators.required]],
-  });
+  empleadoForm?: any;
 
   constructor(
     protected empleadosService: EmpleadosService,
@@ -127,8 +72,6 @@ export class EmpleadosUpdateComponent implements OnInit {
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ empleados }) => {
-      this.updateForm(empleados);
-
       this.empleadosService.query().subscribe((res: HttpResponse<IEmpleados[]>) => (this.empleadosCollection = res.body || []));
 
       this.usuariosService.query().subscribe((res: HttpResponse<IUsuarios[]>) => (this.usuarios = res.body || []));
@@ -148,117 +91,40 @@ export class EmpleadosUpdateComponent implements OnInit {
       this.paisesService.getAll().subscribe((res: HttpResponse<IPaises[]>) => {
         this.paises = res.body || [];
       });
+
+      this.empleadoSeleccionado = empleados;
+
+      console.log(this.empleadoSeleccionado);
+      console.log(this.empleadoSeleccionado.usuario.direccion.direccion.ciudad.provicia.pais.nombre);
+
+      if (this.empleadoSeleccionado !== undefined) {
+        this.formBuilder();
+      }
     });
   }
 
-  updateForm(empleados: IEmpleados): void {
-    this.editForm.patchValue({
-      id: empleados.id,
-      fechaIngreso: empleados.fechaIngreso,
-      jefe: empleados.jefe,
-      usuario: empleados.usuario,
-      estado: empleados.estado,
-      area: empleados.area,
-      puesto: empleados.puesto,
-      fichaje: empleados.fichaje,
-      empresa: empleados.empresa,
+  formBuilder(): void {
+    this.empleadoForm = this.fb.group({
+      id: [this.empleadoSeleccionado.usuario.id],
+      fechaIngreso: [this.empleadoSeleccionado.fechaIngreso],
+      usuario: [this.empleadoSeleccionado.usuario.usuario],
+      nombre: [this.empleadoSeleccionado.usuario.nombre],
+      apellido: [this.empleadoSeleccionado.usuario.apellido],
+      fechaNac: [this.empleadoSeleccionado.usuario.fechaNac],
+      clave: [this.empleadoSeleccionado.usuario.clave],
+      estado: [this.empleadoSeleccionado.usuario.estado],
+      tipoDocumento: [this.empleadoSeleccionado.usuario.contacto.tipoDocumento],
+      pais: [this.empleadoSeleccionado.usuario.direccion.direccion.ciudad.provicia.pais],
+      /* provicia: [this.empleadoSeleccionado.usuario.direccion.direccion.ciudad.provicia]  */
     });
+  }
+
+  actualizarEmpleado(info: any): void {
+    console.log(info);
   }
 
   previousState(): void {
     window.history.back();
-  }
-
-  save(): void {
-    this.isSaving = true;
-    const empleados = this.createFromForm();
-    const pais = this.createPaisFromForm();
-    const provincia = this.createProvinciaFromForm(pais);
-    const ciudad = this.createCiudadesFromForm(provincia);
-    const direcciones = this.createDireccionesFromForm(ciudad);
-
-    const empleado = new EmpleadoRegistroDto(empleados, direcciones, pais, provincia);
-
-    this.subscribeToSaveResponse(this.empleadosService.createEmpReg(empleado));
-  }
-
-  // Creo las instancias que necesito
-
-  private createContactoFromForm(): ITipoContactos {
-    return {
-      ...new TipoContactos(),
-      id: this.contactoForm.get(['id'])?.value,
-      descripcion: this.contactoForm.get(['descripcion'])?.value,
-      tipoDocumento: this.contactoForm.get(['tipoDocumento'])?.value,
-    };
-  }
-
-  private createPaisFromForm(): IPaises {
-    return {
-      ...new Paises(),
-      id: this.paisForm.get(['id'])?.value,
-      nombre: this.paisForm.get(['nombre'])?.value,
-    };
-  }
-
-  private createProvinciaFromForm(pais: Paises): IProvincias {
-    return {
-      ...new Provincias(),
-      id: this.provinciaForm.get(['id'])?.value,
-      nombre: this.provinciaForm.get(['nombre'])?.value,
-      pais,
-    };
-  }
-
-  private createCiudadesFromForm(provincia: Provincias): ICiudades {
-    return {
-      ...new Ciudades(),
-      id: this.ciudadForm.get(['id'])?.value,
-      nombre: this.ciudadForm.get(['nombre'])?.value,
-      provicia: provincia,
-    };
-  }
-
-  private createDireccionesFromForm(ciudad: Ciudades): IDirecciones {
-    return {
-      ...new Direcciones(),
-      id: this.direccionesForm.get(['id'])?.value,
-      calle: this.direccionesForm.get(['calle'])?.value,
-      altura: this.direccionesForm.get(['altura'])?.value,
-      piso: this.direccionesForm.get(['piso'])?.value,
-      departamento: this.direccionesForm.get(['departamento'])?.value,
-      ciudad,
-    };
-  }
-
-  private createUsuarioFromForm(): IUsuarios {
-    return {
-      ...new Usuarios(),
-      id: this.usuarioForm.get(['id'])!.value,
-      nombre: this.usuarioForm.get(['nombre'])!.value,
-      apellido: this.usuarioForm.get(['apellido'])!.value,
-      fechaNac: this.usuarioForm.get(['fechaNac'])!.value,
-      clave: this.usuarioForm.get(['clave'])!.value,
-      usuario: this.usuarioForm.get(['usuario'])!.value,
-      estado: this.usuarioForm.get(['estado'])!.value,
-      direccion: this.usuarioForm.get(['direccion'])!.value,
-      contacto: this.createContactoFromForm(),
-    };
-  }
-
-  private createFromForm(): IEmpleados {
-    return {
-      ...new Empleados(),
-      id: this.editForm.get(['id'])!.value,
-      fechaIngreso: this.editForm.get(['fechaIngreso'])!.value,
-      jefe: this.editForm.get(['jefe'])!.value,
-      usuario: this.createUsuarioFromForm(),
-      estado: this.editForm.get(['estado'])!.value,
-      area: this.editForm.get(['area'])!.value,
-      puesto: this.editForm.get(['puesto'])!.value,
-      fichaje: this.editForm.get(['fichaje'])!.value,
-      empresa: this.editForm.get(['empresa'])!.value,
-    };
   }
 
   protected subscribeToSaveResponse(result: Observable<HttpResponse<IEmpleados>>): void {
@@ -284,43 +150,17 @@ export class EmpleadosUpdateComponent implements OnInit {
     this.isSaving = false;
   }
 
-  setTipoDoc(tipoDoc: any): void {
-    this.contactoForm.controls['tipoDocumentos'].setValue(tipoDoc);
+  trackById(index: number, item: SelectableEntity): any {
+    return item.id;
   }
 
   setProvincias(country: any): void {
-    this.paisForm.controls['nombre'].setValue(country);
+    console.log(country);
+    //this.empleadoForm.pais.controls['nombre'].setValue(country);
     country = this.paises?.filter(x => x.nombre === country);
     this.provinciasService.getByCountry(country[0].id).subscribe((res: HttpResponse<IProvincias[]>) => {
       this.provincias = res.body || [];
     });
-  }
-
-  setCiudades(state: any): void {
-    this.ciudadForm.controls['nombre'].setValue(state);
-  }
-
-  setProvForm(provincia: any): void {
-    provincia = this.provincias.filter((x: any) => x.nombre === provincia);
-    this.ciudadesService.getByState(provincia[0].id).subscribe((res: HttpResponse<ICiudades[]>) => {
-      this.ciudades = res.body || [];
-    });
-
-    this.provinciaForm.controls['nombre'].setValue(provincia[0].nombre);
-  }
-
-  setAltura(altura: any): void {
-    this.direccionesForm.controls['altura'].setValue(altura);
-  }
-  setPiso(piso: any): void {
-    this.direccionesForm.controls['piso'].setValue(piso);
-  }
-  setDepartamento(depto: any): void {
-    this.direccionesForm.controls['departamento'].setValue(depto);
-  }
-
-  trackById(index: number, item: SelectableEntity): any {
-    return item.id;
   }
 }
 /* eslint-enable no-console */
